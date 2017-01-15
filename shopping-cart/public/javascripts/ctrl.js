10d4ecb233fe1d8b8edd6a5513c5f762e566f82b
Rahul <Rahul>
2017-01-15 13:16:13 +0530

    repeat and templating

---
+++ shopping-cart/public/javascripts/ctrl.js
@@ -0,0 +1,16 @@
+// function getItem(){
+//     var item = $.deferred();
+//     $.ajax({
+//         url:"../../views/item.html",
+//         success:function(data){
+         
+//             item.resolve(data);
+//         },
+//         error:function(error){
+//             item.reject(error);
+//         }
+//     });
+
+//     return item.promise();
+
+// }
